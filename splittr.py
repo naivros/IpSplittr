a0c7d397c785918b945018e0c9569d423ed4d0c0
naivros <payton.miller404@gmail.com>
2020-10-20 20:12:14 -0300

    Update splittr.py

--- splittr.py
+++ splittr.py
@@ -3,81 +3,103 @@ import sys
 
 axiomCount = int(sys.argv[1])
 
-map = {} # int --> array
-
-file = open('tmp', 'r')
+file = open('ips.txt', 'r')
 line = file.readlines()
 
 isPrivate = 0
 totalIP = 0
+class Storage():
+    def __init__(self):
+        self.map = {}
+        self.isPrivate = 0;
+        self.totalIP = 0;
+        self.axiomCount = int(sys.argv[1])
+    def incPrivateIP(self):
+        self.isPrivate = self.isPrivate + 1
+    def incTotalIP(self):
+        self.totalIP = self.totalIP + 1
+    def processSubnets(self, iprange, subnet):
+        ipCount = len(list(subnet))
+        if(ipCount in self.map):
+          self.map[ipCount].append(iprange)
+        else:
+          self.map[ipCount] = [iprange]
+        for ip in subnet:
+          self.totalIP += 1
+          if(ip.is_private):
+            self.isPrivate += 1
+    def getMap(self):
+        return self.map;
+    def getIPCount(self):
+        return self.totalIP;
+    def getPrivateCount(self):
+        return self.isPrivate
+    def getAxiom(self):
+        return self.axiomCount
+
+Storage = Storage()
+
+def processData(Storage):
+  map = Storage.getMap()
+  lists = {}
+
+  for i in range(1, Storage.getAxiom()+1):
+    goal = Storage.getIPCount() // Storage.getAxiom() + 1
+    current = 0
+  #print("Target", goal)
+    for key in sorted(map.keys(),reverse=True):
+      while key + current < goal and len(map[key]) > 0:
+        cache = map[key]
+
+        if not i in lists:
+          lists[i] = [cache[0]]
+        else:
+          lists[i].append(cache[0])
+        cache.pop(0)
+        map[key] = cache
+        current += key
+        #print(current)
+  return lists
+
+
 
 for iprange in line:
-  #Clean
-  iprange = iprange.replace("\n", "")
+  #Clean Input
+  iprange = iprange .replace("\n", "")
   iprange = iprange.replace(" ", "")
 
   if (iprange != ""):
-
-    subnet=list(ipaddress.ip_network(iprange, False).hosts())
-    ipCount = len(subnet)
-    if ipCount == 0:
-        ipCount = 1
-    if(ipCount in map):
-      map[ipCount].append(iprange)
+    if int(iprange[(iprange.find("/", 0))+1:]) < 28:
+        networks=list(ipaddress.ip_network(iprange, False).subnets(new_prefix=28))
+        for smallerNet in networks:
+            snet=list(ipaddress.ip_network(str(smallerNet)))
+            Storage.processSubnets(str(smallerNet),snet)
     else:
-      map[ipCount] = [iprange]
+        subnet=list(ipaddress.ip_network(iprange, False).hosts())
+        Storage.processSubnets(iprange,subnet)
 
-    for ip in subnet:
-      totalIP += 1
-      if(ip.is_private):
-        isPrivate += 1
+map = Storage.getMap()
 
 print("*"*72)
-print("**** Total IPs: ", totalIP)
-print("**** Private IPs: ", isPrivate)
+print("**** Total IPs: ", Storage.getIPCount())
+print("**** Private IPs: ", Storage.getPrivateCount())
 print("*"*72)
 
 for a in sorted(map.keys(), reverse=True):
-  print("* ::", len(map[a]), "Subnet ::", a,"IPs Per Subnet ::", a*len(map[a]), "Total IPs", "::")
+  print("* ::", len(map[a]), "Subnets ::", a,"IPs Per Subnet ::", a*len(map[a]), "Total IPs", "::")
 print("*"*72)
 
-lists = {} # map(integer) => array of IPRanges
-for i in range(1, axiomCount+1):
-  goal = totalIP // axiomCount + 1
-  current = 0
-  #print("Target", goal)
-  for key in sorted(map.keys(),reverse=True):
-    while key + current < goal and len(map[key]) > 0:
-      cache = map[key]
-      if not i in lists:
-        lists[i] = [cache[0]]
-      else:
-        lists[i].append(cache[0])
-      cache.pop(0)
-      map[key] = cache
-      current += key
-      #print(current)
+map = Storage.getMap()
 
-## ROUND ROBIN DISTRIBUTION
-for key in sorted(map.keys(), reverse=True):
-  while len(map[key]) > 0:
-    if(i>axiomCount):
-      i=1
-    cache = map[key]
-    lists[i].append(cache[0])
-    cache.pop(0)
+lists = processData(Storage)
 
-    map[key] = cache
-    current += key
-    i = i + 1
-#At the end of this every map[key] == []
-
-## // TESTCASE- TEST COUNTING DIVIDED IPS TO ENSURE THAT THEY'RE EQUALLY DISTRIBUTED
 def test(lists):
   for key in sorted(lists.keys()):
     count = 0
     for iprange in (lists[key]):
       x=list(ipaddress.ip_network(iprange, False).hosts())
+      if x == []:
+          x=list(ipaddress.ip_network(iprange, False))
       count = count + len(x) + 1
     print("* ::", key, "::",count, "ips ::")
   print("*"*72)
